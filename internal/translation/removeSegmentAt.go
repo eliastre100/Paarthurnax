0cd2b7fd9ec5a11099f4151629d1b512daa1ed82
Anaïs FORET <anais@foret.dev>
2024-08-06 09:04:33 +0200

    Fix deletion of nested keys

--- internal/translation/removeSegmentAt.go
+++ internal/translation/removeSegmentAt.go
@@ -10,7 +10,7 @@ func (translation *TranslationFile) RemoveSegmentAt(path string) error {
 
 func removeSegmentAt(m map[string]interface{}, path string) error {
 	pathSegments := strings.Split(path, ".")
-	if len(pathSegments) != 1 {
+	if len(pathSegments) != 1 { // Trunk
 		newM, ok := m[pathSegments[0]].(map[string]interface{})
 		if !ok {
 			return nil // The key does not exist already
@@ -19,7 +19,12 @@ func removeSegmentAt(m map[string]interface{}, path string) error {
 		if err := removeSegmentAt(newM, strings.Join(pathSegments[1:], ".")); err != nil {
 			return err
 		}
+
+		if len(newM) == 0 {
+			delete(m, pathSegments[0])
+		}
+	} else if len(pathSegments) == 1 { // Leaf
+		delete(m, pathSegments[0])
 	}
-	delete(m, pathSegments[0])
 	return nil
 }
